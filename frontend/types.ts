c4e29a1d90e74ffecea48adac0eea7dedf848d0c
anonimus <148051645+cosminnionutt@users.noreply.github.com>
2024-03-06 16:46:27 +0200

    Add files via upload

---
+++ frontend/types.ts
@@ -0,0 +1,36 @@
+import { Stack } from "./lib/stacks";
+import { CodeGenerationModel } from "./lib/models";
+
+export enum EditorTheme {
+  ESPRESSO = "espresso",
+  COBALT = "cobalt",
+}
+
+export interface Settings {
+  openAiApiKey: string | null;
+  openAiBaseURL: string | null;
+  screenshotOneApiKey: string | null;
+  isImageGenerationEnabled: boolean;
+  editorTheme: EditorTheme;
+  generatedCodeConfig: Stack;
+  codeGenerationModel: CodeGenerationModel;
+  // Only relevant for hosted version
+  isTermOfServiceAccepted: boolean;
+  accessCode: string | null;
+}
+
+export enum AppState {
+  INITIAL = "INITIAL",
+  CODING = "CODING",
+  CODE_READY = "CODE_READY",
+}
+
+export interface CodeGenerationParams {
+  generationType: "create" | "update";
+  image: string;
+  resultImage?: string;
+  history?: string[];
+  isImportedFromCode?: boolean;
+}
+
+export type FullGenerationSettings = CodeGenerationParams & Settings;
